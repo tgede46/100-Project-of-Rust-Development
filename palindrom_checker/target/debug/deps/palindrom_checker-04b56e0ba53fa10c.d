c07c153645a6a4ab7f249edf9b6199319fef908f
tgede46 <gedeonkpara@gmail.com>
2026-03-06 22:49:59 +0000

    (feat) project 7: PALINDROM CHECKER

---
+++ palindrom_checker/target/debug/deps/palindrom_checker-04b56e0ba53fa10c.d
@@ -0,0 +1,5 @@
+/home/gedeonkp/Tool/ToolRust/100-Project-of-Rust-Development/palindrom_checker/target/debug/deps/palindrom_checker-04b56e0ba53fa10c.d: src/main.rs
+
+/home/gedeonkp/Tool/ToolRust/100-Project-of-Rust-Development/palindrom_checker/target/debug/deps/libpalindrom_checker-04b56e0ba53fa10c.rmeta: src/main.rs
+
+src/main.rs:
---
+++ palindrom_checker/target/debug/deps/palindrom_checker-724b1757ab3e0cb0.d
@@ -0,0 +1,5 @@
+/home/gedeonkp/Tool/ToolRust/100-Project-of-Rust-Development/palindrom_checker/target/debug/deps/palindrom_checker-724b1757ab3e0cb0.d: src/main.rs
+
+/home/gedeonkp/Tool/ToolRust/100-Project-of-Rust-Development/palindrom_checker/target/debug/deps/libpalindrom_checker-724b1757ab3e0cb0.rmeta: src/main.rs
+
+src/main.rs:
